d9d63a34f6b4d6780ffe831cb662e748a80f6e75
Emanuel Ben Hamo <embenh@gmail.com>
2020-04-30 19:21:06 +0300

    changed input field to work with elemen io - css framework for vue js

--- perion-top-suggestion-client/src/main.js
+++ perion-top-suggestion-client/src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from "./App.vue";
 
 import './styles/global.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App)
